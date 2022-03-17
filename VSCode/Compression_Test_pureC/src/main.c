125bead12d61230670aac608724db19ea10ce775
Thegaardenator <sandergaarden@gmail.com>
2022-03-17 15:49:11 +0100

    Additional test code added.

--- VSCode/Compression_Test_pureC/src/main.c
+++ VSCode/Compression_Test_pureC/src/main.c
@@ -1,9 +1,37 @@
+/*
+ * Checkliste til brug af bibliotek
+ * Indsæt opløsning i variabler (xRes, yRes)
+ * Definer datalængde udfra variabler (dataLen = xRes * yRes * 4)
+ * 
+ * 
+ * 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+// #include "../lib/SatCamImage.h"
+#include "SatCamImage.h"
+
 int main() {
-	FILE* fInput = fopen("C:\\Users\\sande\\Documents\\PlatformIO\\Projects\\Compression_Test_pureC\\src\\inputfile.txt", "r");
-	FILE* fOutput = fopen("C:\\Users\\sande\\Documents\\PlatformIO\\Projects\\Compression_Test_pureC\\src\\file.txt", "w");
+	BYTE xDensity[2] = {0x01, 0x2c};
+	BYTE yDensity[2] = {0x01, 0x2c};
+	
+	JFIFHEADER header;
+	BuildJFIFHeader(&header, 0x00, xDensity, yDensity, 0x00, 0x00);
+
+	for(unsigned int i = 0; i<sizeof(header); i++) {
+		BYTE charToWrite = &header + i;
+		printf("%#08x\n", charToWrite);
+	}
+
+	return 0;
+}
+
+/* 
+int main() {
+	FILE* fInput = fopen("ahaha.png", "rb");
+	FILE* fOutput = fopen("output.png", "wb");
 
 	if(fInput == NULL){
 		printf("\nError opening file.\n");
@@ -40,7 +68,7 @@ int main() {
 	fclose(fOutput);
 
 	return 0;
-}
+}*/
 
 /*
 int main() {
